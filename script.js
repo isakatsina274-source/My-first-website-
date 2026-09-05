70a6111eff8f10942ed815d8f3c082244a846977
isakatsina274-source <isakatsina274@gmail.com>
2026-09-05 09:44:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const button = document.querySelector("button");
 const heading = document.querySelector(".title");
 const message = document.querySelector("p");
+const subtitle = document.querySelector(".subtitle");
 
 let clicked = false;
 
@@ -8,6 +9,7 @@ button.addEventListener("click", function () {
  if (clicked === false) {
     heading.textContent = "Welcome to My World!";
     message.textContent = "You just changed this webpage with JavaScript!";
+  subtitle.textContent = "I am becoming a frontend developer!";
      clicked = true;
 } else {
      heading.textContent = "Welcome to Isa’s Website";
