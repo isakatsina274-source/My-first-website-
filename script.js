09d0baee66ebdc4fbb9f55418abd8b8516450132
isakatsina274-source <isakatsina274@gmail.com>
2026-09-05 08:54:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const button = document.querySelector("button");
 const heading = document.querySelector(".title");
 const message = document.querySelector("p");
-
+let clicked = false;
 button.addEventListener("click", function () {
     heading.textContent = "Welcome to My World!";
     message.textContent = "You just changed this webpage with JavaScript!";
