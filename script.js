40bd2293aaf826dff0dfc511b10c91d1b005e62f
isakatsina274-source <isakatsina274@gmail.com>
2026-09-05 09:06:19 +0100

    Fix conditional check for button click event

--- script.js
+++ script.js
@@ -5,5 +5,12 @@ let clicked = false;
 button.addEventListener("click", function () {
     heading.textContent = "Welcome to My World!";
     message.textContent = "You just changed this webpage with JavaScript!";
-    if (clicked ______ false) {
+    if (clicked === false) {
+    heading.textContent = "Welcome to My World!";
+    clicked = true;
+}
+    else {
+    heading.textContent = "Welcome to Isa’s Website";
+    clicked = false;
+}
 });
