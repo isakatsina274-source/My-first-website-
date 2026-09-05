f478e0284baee76872436a1539133fa42ca3eead
isakatsina274-source <isakatsina274@gmail.com>
2026-09-05 09:12:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const button = document.querySelector("button");
 const heading = document.querySelector(".title");
 const message = document.querySelector("p");
+
 let clicked = false;
+
 button.addEventListener("click", function () {
+ if (clicked === false) {
     heading.textContent = "Welcome to My World!";
     message.textContent = "You just changed this webpage with JavaScript!";
-    if (clicked === false) {
-    heading.textContent = "Welcome to My World!";
-    clicked = true;
-}
-    else {
-    heading.textContent = "Welcome to Isa’s Website";
-    clicked = false;
-}
+     clicked = true;
+} else {
+     heading.textContent = "Welcome to Isa’s Website";
+        message.textContent = "Click the button to change the page!";
+        clicked = false;
+    }
 });
