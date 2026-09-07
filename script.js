dceceabc20cfefdd27bd9550545f3df1ec61901c
isakatsina274-source <isakatsina274@gmail.com>
2026-09-07 09:29:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -43,4 +43,14 @@ button.addEventListener("click", function () {
         resetSubtitle();
         clicked = false;
     }
+ async function getCrop() {
+
+    const response = await fetch("https://dummyjson.com/products/1");
+
+    const data = await response.json();
+
+    console.log(data);
+}
+
+getCrop();   
 });
