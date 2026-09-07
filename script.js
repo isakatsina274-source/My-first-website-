cb637d43288f901c4a8cb564db983f93388dcbe1
isakatsina274-source <isakatsina274@gmail.com>
2026-09-07 12:14:03 +0100

    Update fetch URL in getCrop function

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ async function getCrop() {
 
     try {
     
-    const response = await fetch("https://dummyjson.com/products/19999");
+    const response = await fetch("https://dummyjson.com/products/9999");
         console.log(response.status);
         
 if (!response.ok) {
